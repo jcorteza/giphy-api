41b2d965a765dac351e04e01d788cb48ca8902f9
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-09-21 19:17:28 -0500

    Made suggested changes

--- assets/api.js
+++ assets/api.js
@@ -1,6 +1,6 @@
 var showsArray = ["Friends", "Game of Thrones", "Parks and Recreation", "The Office", "The Goldbergs", "Jane the Virgin", "Supergirl", "Golden Girls", "The Flash", "Riverdale", "The Good Place", "Once Upon a Time"]
 
-
+$(document).ready(function() {
 /* this for loop creates a button for each item in showsArray, sets its text to the current item text, and appends it to #btnsDiv*/
 for(i = 0; i < showsArray.length; i++) {
     let showName = showsArray[i];
@@ -40,22 +40,22 @@ $("#btnsDiv").on("click", function(event){
             $(ratingText).prependTo(gifDiv);
         }
     });
-    $("#gifsHolderDiv").off("click").on("click", function(event) {
+    $("#gifsHolderDiv")/*.off("click")*/.on("click", ".gifResult", function(event) {
         console.log("Inside gifsHolderDiv function.");
         if($(event.target).attr("type") === "still") {
             $(event.target).attr("type", "gif");
-            let stillGif = $(event.target).attr("src");
-            let animatedGif = $(event.target).attr("subLink");
+            const stillGif = $(event.target).attr("src");
+            const animatedGif = $(event.target).attr("subLink");
             $(event.target).attr("src", animatedGif);
             $(event.target).attr("subLink", stillGif);
         }
         else if ($(event.target).attr("type") === "gif") {
             $(event.target).attr("type", "still");
-            let animatedGif = $(event.target).attr("src");
-            let stillGif = $(event.target).attr("subLink");
+            const animatedGif = $(event.target).attr("src");
+            const stillGif = $(event.target).attr("subLink");
             $(event.target).attr("src", stillGif);
             $(event.target).attr("subLink", animatedGif);
         }
     });
 });
-
+});
